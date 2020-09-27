f2a5536ca215403dc223d93ff9b8d761f6d0f807
cool5tar <cool5tarxv@gmail.com>
2020-09-27 19:43:26 +0800

    made v1.0 restoreX

---
+++ restoreX.bat
@@ -0,0 +1,17 @@
+echo made by sn0wstar
+
+echo starting...
+
+sleep 1
+
+echo creating restore point...
+
+echo launching powershell...
+
+sleep 1
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -NoExit -Command "Checkpoint-Computer -Description "FALLGUYS-restoreX" -RestorePointType "MODIFY_SETTINGS"; " ' " -Verb RunAs}"
+
+echo RESTORE POINT CREATED
+
+echo THANK YOU FOR USING restoreX
