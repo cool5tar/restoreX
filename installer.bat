61b0068560aceee3faa50f7e5ade6939585c5ce6
cool5tar <cool5tarxv@gmail.com>
2020-09-28 17:55:30 +0800

    addedd full uninst

--- installer.bat
+++ installer.bat
@@ -5,4 +5,5 @@ cd restoreX
 curl https://raw.githubusercontent.com/cool5tar/restoreX/master/restoreX.bat --output restoreX.bat
 curl https://raw.githubusercontent.com/cool5tar/restoreX/master/update.bat --ouput update.bat
 curl https://raw.githubusercontent.com/cool5tar/restoreX/master/upd.bat --ouput upd.bat
+curl https://raw.githubusercontent.com/cool5tar/restoreX/master/uninst.bat --output uninst.bat
 restoreX.bat
