303574f0052d0d608d7153ead01c2ccd298146df
cool5tar <cool5tarxv@gmail.com>
2020-09-28 18:44:32 +0800

    added power1

--- installer.bat
+++ installer.bat
@@ -4,7 +4,7 @@ mkdir restoreX
 cd restoreX
 curl https://raw.githubusercontent.com/cool5tar/restoreX/master/restoreX.bat --output restoreX.bat
 curl https://raw.githubusercontent.com/cool5tar/restoreX/master/update.bat --ouput update.bat
-curl https://raw.githubusercontent.com/cool5tar/restoreX/master/upd.bat --ouput upd.bat
-curl https://raw.githubusercontent.com/cool5tar/restoreX/master/uninst.bat --output uninst.bat
+curl https://raw.githubusercontent.com/cool5tar/restoreX/master/upd.bat 
+curl https://raw.githubusercontent.com/cool5tar/restoreX/master/uninst.bat 
 powershell
 c:/restoreX/restoreX.bat
